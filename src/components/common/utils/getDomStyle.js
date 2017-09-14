20a62b71ad9cbfae8743932f3c2ec114206aa8c1
Yidadaa <1143922395@qq.com>
2017-09-14 22:30:48 +0800

    remove files

--- src/components/common/utils/getDomStyle.js
+++
@@ -1,6 +0,0 @@
-/**
- * 工具方法，获取某个dom的实时style
- */
-module.exports = (domName) => {
-  return window.getComputedStyle(document.querySelector(domName));
-};
--- src/routes/Report.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Report = () => {
-  return <div>reports</div>;
-};
-
-export default Report;
